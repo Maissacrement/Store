d6be62da7a0aecb8c5dcc2ae2d00792c58e52740
mario <emmariodelar@gmail.com>
2019-10-10 21:34:20 +0200

    Add angular

--- single-spa.config.js
+++ single-spa.config.js
@@ -1,13 +1,32 @@
 import {registerApplication, start} from 'single-spa'
 
+function pathPrefix(prefix) {
+  return function(location) {
+    return location.pathname.startsWith(prefix);
+  }
+}
+
 registerApplication(
   // Name of our single-spa application
   'home',
   // Our loading function
   () => import('./src/home/home.app.js'),  // Our activity function
-  () => location.pathname === "" || 
+  () => location.pathname === "" ||
     location.pathname === "/" ||
     location.pathname.startsWith('/home')
+);
+
+registerApplication(
+  'navBar',
+  () => import('./src/navBar/navBar.app.js')
+          .then(module => module.navBar),
+  () => true
+);
+
+registerApplication(
+  'angularJS',
+  () => import ('./src/angularJS/angularJS.app.js'),
+  pathPrefix('/angularJS')
 );
 
 start()
